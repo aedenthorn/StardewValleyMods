efc66ac2dc611f4e80b7987d2b2ac8e33366b951
aedenthorn <aedenthorn1@gmail.com>
2021-12-30 21:42:31 -0500

    plant all fertilizer

--- PlantAll/CodePatches.cs
+++ PlantAll/CodePatches.cs
@@ -10,7 +10,7 @@ namespace PlantAll
 
         private static void Utility_tryToPlaceItem_Postfix(GameLocation location, Item item, int x, int y, bool __result)
         {
-            if (!Config.EnableMod || !__result || !SHelper.Input.IsDown(Config.ModButton) || !(item is Object) || ((Object)item).Category != -74 || item.Stack <= 0)
+            if (!Config.EnableMod || !__result || !SHelper.Input.IsDown(Config.ModButton) || !(item is Object) || (((Object)item).Category != -74 && ((Object)item).Category != -19) || item.Stack <= 0)
                 return;
             SMonitor.Log($"Planting all");
 
@@ -24,7 +24,7 @@ namespace PlantAll
             {
                 if(((Object)Game1.player.CurrentItem).placementAction(Game1.player.currentLocation, p.X * 64, p.Y * 64, Game1.player))              
                     Game1.player.reduceActiveItemByOne();
-                if (Game1.player.CurrentItem == null || Game1.player.CurrentItem.Category != -74 || Game1.player.CurrentItem.Stack <= 0)
+                if (Game1.player.CurrentItem == null || (Game1.player.CurrentItem.Category != -74 && Game1.player.CurrentItem.Category != -19) || Game1.player.CurrentItem.Stack <= 0)
                     return;
             }
 
@@ -62,7 +62,7 @@ namespace PlantAll
             }
             for (int i = tiles.Count - 1; i >= 0; i--)
             {
-                if (!Game1.player.CurrentItem.canBePlacedHere(Game1.player.currentLocation, new Vector2(tiles[i].X, tiles[i].Y)) || !Utility.isViableSeedSpot(Game1.player.currentLocation, new Vector2(tiles[i].X, tiles[i].Y), Game1.player.CurrentItem))
+                if (!Game1.player.CurrentItem.canBePlacedHere(Game1.player.currentLocation, new Vector2(tiles[i].X, tiles[i].Y)) || (!SHelper.Reflection.GetMethod(typeof(Utility), "itemCanBePlaced").Invoke<bool>(new object[] { Game1.player.currentLocation, new Vector2(tiles[i].X, tiles[i].Y), Game1.player.CurrentItem }) && !Utility.isViableSeedSpot(Game1.player.currentLocation, new Vector2(tiles[i].X, tiles[i].Y), Game1.player.CurrentItem)))
                 {
                     tiles.RemoveAt(i);
                 }
