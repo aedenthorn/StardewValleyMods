62fb9451a132005a8628613d0a4576349102480b
aedenthorn <aedenthorn1@gmail.com>
2022-03-22 18:38:41 -0400

    etc

---
+++ ConnectedGardenPots/ModEntry.cs
@@ -0,0 +1,74 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Locations;
+using StardewValley.Objects;
+using Object = StardewValley.Object;
+
+namespace ConnectedGardenPots
+{
+    /// <summary>The mod entry point.</summary>
+    public partial class ModEntry : Mod
+    {
+
+        public static IMonitor SMonitor;
+        public static IModHelper SHelper;
+        public static ModConfig Config;
+        public static ModEntry context;
+
+        private static Texture2D gardenPotspriteSheet;
+
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            Config = Helper.ReadConfig<ModConfig>();
+
+            context = this;
+
+            SMonitor = Monitor;
+            SHelper = helper;
+
+            helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
+            helper.Events.GameLoop.SaveLoaded += GameLoop_SaveLoaded;
+            var harmony = new Harmony(ModManifest.UniqueID);
+            harmony.PatchAll();
+        }
+
+        private void GameLoop_SaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            try
+            {
+                gardenPotspriteSheet = Game1.content.Load<Texture2D>("aedenthorn.ConnectedGardenPots/sprite_sheet");
+            }
+            catch
+            {
+                gardenPotspriteSheet = Helper.Content.Load<Texture2D>("assets/sprite_sheet.png");
+            }
+        }
+        private void GameLoop_GameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+
+            // get Generic Mod Config Menu's API (if it's installed)
+            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+                return;
+
+            // register mod
+            configMenu.Register(
+                mod: ModManifest,
+                reset: () => Config = new ModConfig(),
+                save: () => Helper.WriteConfig(Config)
+            );
+
+            configMenu.AddBoolOption(
+                mod: ModManifest,
+                name: () => "Mod Enabled",
+                getValue: () => Config.EnableMod,
+                setValue: value => Config.EnableMod = value
+            );
+        }
+    }
+}
