e22be5516f81f37ffacee42df3a74691d8f027e5
aedenthorn <aedenthorn1@gmail.com>
2022-07-27 14:52:17 -0400

    catalogue filter

---
+++ CatalogueFilter/CodePatches.cs
@@ -0,0 +1,108 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using StardewValley;
+using StardewValley.BellsAndWhistles;
+using StardewValley.Menus;
+using System;
+using System.Collections.Generic;
+
+namespace CatalogueFilter
+{
+    public partial class ModEntry
+    {
+        private static string lastFilterString = "";
+        private static TextBox filterField;
+        private static List<ISalable> allItems;
+
+        [HarmonyPatch(typeof(ShopMenu), new Type[] { typeof(List<ISalable>), typeof(int), typeof(string), typeof(Func<ISalable, Farmer, int, bool>), typeof(Func<ISalable, bool>), typeof(string) })]
+        [HarmonyPatch(MethodType.Constructor)]
+        public class ShopMenu_Patch
+        {
+            public static void Postfix(ShopMenu __instance, List<ISalable> itemsForSale)
+            {
+                if (!Config.ModEnabled)
+                    return;
+                allItems = new List<ISalable>(__instance.forSale);
+                filterField = new TextBox(Game1.content.Load<Texture2D>("LooseSprites\\textBox"), null, Game1.smallFont, Game1.textColor)
+                {
+                    X = __instance.xPositionOnScreen + 28,
+                    Y = __instance.yPositionOnScreen + __instance.height - 88,
+                    Text = ""
+                };
+            }
+        }
+        
+        [HarmonyPatch(typeof(ShopMenu), nameof(ShopMenu.drawCurrency))]
+        public class ShopMenu_drawCurrency_Patch
+        {
+
+            public static void Postfix(ShopMenu __instance, SpriteBatch b)
+            {
+                if (!Config.ModEnabled)
+                    return;
+                if(lastFilterString != filterField.Text)
+                {
+                    lastFilterString = filterField.Text;
+                    ChangeItemList(__instance);
+                    __instance.gameWindowSizeChanged(Game1.graphics.GraphicsDevice.Viewport.Bounds, Game1.graphics.GraphicsDevice.Viewport.Bounds);
+                }
+                filterField.Draw(b);
+                if (Config.ShowLabel)
+                {
+                    SpriteText.drawStringHorizontallyCenteredAt(b, SHelper.Translation.Get("filter"), __instance.xPositionOnScreen + 128, __instance.yPositionOnScreen + __instance.height - 136, 999999, -1, 999999, 1, 0.88f, false, Config.LabelColor, 99999);
+                }
+            }
+        }
+        
+        [HarmonyPatch(typeof(ShopMenu), nameof(ShopMenu.receiveLeftClick))]
+        public class ShopMenu_receiveLeftClick_Patch
+        {
+
+            public static void Postfix(ShopMenu __instance)
+            {
+                if (!Config.ModEnabled)
+                    return;
+                filterField.Update();
+            }
+        }
+        [HarmonyPatch(typeof(ShopMenu), nameof(ShopMenu.receiveKeyPress))]
+        public class ShopMenu_receiveKeyPress_Patch
+        {
+
+            public static bool Prefix(ShopMenu __instance, Keys key)
+            {
+                if (!Config.ModEnabled || !filterField.Selected || key == Keys.Escape)
+                    return true;
+                return false;
+            }
+        }
+        [HarmonyPatch(typeof(ShopMenu), nameof(ShopMenu.performHoverAction))]
+        public class ShopMenu_performHoverAction_Patch
+        {
+
+            public static void Postfix(ShopMenu __instance, int x, int y)
+            {
+                if (!Config.ModEnabled)
+                    return;
+                filterField.Hover(x, y);
+            }
+        }
+
+        private static void ChangeItemList(ShopMenu shopMenu)
+        {
+            shopMenu.forSale.Clear();
+            if (filterField.Text == "")
+            {
+                shopMenu.forSale.AddRange(allItems);
+                return;
+            }
+            foreach (var i in allItems)
+            {
+                if (i.DisplayName.ToLower().Contains(filterField.Text.ToLower()))
+                    shopMenu.forSale.Add(i);
+            }
+        }
+    }
+}
---
+++ CatalogueFilter/ModConfig.cs
@@ -0,0 +1,12 @@
+﻿using StardewModdingAPI;
+using System.Collections.Generic;
+
+namespace CatalogueFilter
+{
+    public class ModConfig
+    {
+        public bool ModEnabled { get; set; } = true;
+        public bool ShowLabel { get; set; } = true;
+        public int LabelColor { get; set; } = 4;
+    }
+}
