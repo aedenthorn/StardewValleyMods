a467402289a736190794cf803626f5b460be6fb4
aedenthorn <aedenthorn1@gmail.com>
2022-09-08 20:58:37 -0400

    etc

--- SocialPageOrderMenu/ModEntry.cs
+++ SocialPageOrderMenu/ModEntry.cs
@@ -33,9 +33,46 @@ namespace SocialPageOrderMenu
             SHelper = Helper;
 
             helper.Events.Input.ButtonPressed += Input_ButtonPressed;
+            helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
 
             var harmony = new Harmony(ModManifest.UniqueID);
             harmony.PatchAll();
+        }
+
+        private void GameLoop_GameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+
+            // get Generic Mod Config Menu's API (if it's installed)
+            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+                return;
+
+            // register mod
+            configMenu.Register(
+                mod: ModManifest,
+                reset: () => Config = new ModConfig(),
+                save: () => Helper.WriteConfig(Config)
+            );
+
+            configMenu.AddBoolOption(
+                mod: ModManifest,
+                name: () => "Mod Enabled?",
+                getValue: () => Config.EnableMod,
+                setValue: value => Config.EnableMod = value
+            );
+
+            configMenu.AddKeybind(
+                mod: ModManifest,
+                name: () => "Prev Sort Key",
+                getValue: () => Config.prevButton,
+                setValue: value => Config.prevButton = value
+            );
+            configMenu.AddKeybind(
+                mod: ModManifest,
+                name: () => "Next Sort Key",
+                getValue: () => Config.nextButton,
+                setValue: value => Config.nextButton = value
+            );
         }
 
         private void Input_ButtonPressed(object sender, ButtonPressedEventArgs e)
