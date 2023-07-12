0f01affe11845fffa7876842890c7088648f1723
aedenthorn <aedenthorn1@gmail.com>
2023-07-12 12:20:09 -0400

    etc

--- Arabic/CodePatches.cs
+++ Arabic/CodePatches.cs
@@ -14,49 +14,49 @@ namespace Arabic
         [HarmonyPatch(typeof(SpriteBatch), nameof(SpriteBatch.DrawString), new Type[] {typeof(SpriteFont), typeof(string), typeof(Vector2), typeof(Color) })]
         public class SpriteBatch_Patch1
         {
-            public static void Prefix(ref SpriteFont spriteFont, ref string text)
+            public static void Prefix(ref SpriteFont spriteFont, ref string text, ref Vector2 position)
             {
-                FixForArabic(ref spriteFont, ref text);
+                FixForArabic(ref spriteFont, ref text, ref position);
             }
         }
         [HarmonyPatch(typeof(SpriteBatch), nameof(SpriteBatch.DrawString), new Type[] {typeof(SpriteFont), typeof(StringBuilder), typeof(Vector2), typeof(Color) })]
         public class SpriteBatch_Patch2
         {
-            public static void Prefix(ref SpriteFont spriteFont, ref string text)
+            public static void Prefix(ref SpriteFont spriteFont, ref StringBuilder text, ref Vector2 position)
             {
-                FixForArabic(ref spriteFont, ref text);
+                FixForArabic(ref spriteFont, ref text, ref position);
             }
         }
         [HarmonyPatch(typeof(SpriteBatch), nameof(SpriteBatch.DrawString), new Type[] {typeof(SpriteFont), typeof(string), typeof(Vector2), typeof(Color), typeof(float), typeof(Vector2), typeof(float), typeof(SpriteEffects), typeof(float) })]
         public class SpriteBatch_Patch3
         {
-            public static void Prefix(ref SpriteFont spriteFont, ref string text)
+            public static void Prefix(ref SpriteFont spriteFont, ref string text, ref Vector2 position)
             {
-                FixForArabic(ref spriteFont, ref text);
+                FixForArabic(ref spriteFont, ref text, ref position);
             }
         }
         [HarmonyPatch(typeof(SpriteBatch), nameof(SpriteBatch.DrawString), new Type[] {typeof(SpriteFont), typeof(string), typeof(Vector2), typeof(Color), typeof(float), typeof(Vector2), typeof(Vector2), typeof(SpriteEffects), typeof(float) })]
         public class SpriteBatch_Patch4
         {
-            public static void Prefix(ref SpriteFont spriteFont, ref string text)
+            public static void Prefix(ref SpriteFont spriteFont, ref string text, ref Vector2 position)
             {
-                FixForArabic(ref spriteFont, ref text);
+                FixForArabic(ref spriteFont, ref text, ref position);
             }
         }
         [HarmonyPatch(typeof(SpriteBatch), nameof(SpriteBatch.DrawString), new Type[] {typeof(SpriteFont), typeof(StringBuilder), typeof(Vector2), typeof(Color), typeof(float), typeof(Vector2), typeof(float), typeof(SpriteEffects), typeof(float) })]
         public class SpriteBatch_Patch5
         {
-            public static void Prefix(ref SpriteFont spriteFont, ref string text)
+            public static void Prefix(ref SpriteFont spriteFont, ref StringBuilder text, ref Vector2 position)
             {
-                FixForArabic(ref spriteFont, ref text);
+                FixForArabic(ref spriteFont, ref text, ref position);
             }
         }
         [HarmonyPatch(typeof(SpriteBatch), nameof(SpriteBatch.DrawString), new Type[] {typeof(SpriteFont), typeof(StringBuilder), typeof(Vector2), typeof(Color), typeof(float), typeof(Vector2), typeof(Vector2), typeof(SpriteEffects), typeof(float) })]
         public class SpriteBatch_Patch6
         {
-            public static void Prefix(ref SpriteFont spriteFont, ref string text)
+            public static void Prefix(ref SpriteFont spriteFont, ref StringBuilder text, ref Vector2 position)
             {
-                FixForArabic(ref spriteFont, ref text);
+                FixForArabic(ref spriteFont, ref text, ref position);
             }
         }
     }
--- Arabic/Methods.cs
+++ Arabic/Methods.cs
@@ -1,18 +1,32 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using StardewValley;
+using System.Text;
 
 namespace Arabic
 {
     public partial class ModEntry
     {
-        private static void FixForArabic(ref SpriteFont spriteFont, ref string text)
+        private static void FixForArabic(ref SpriteFont spriteFont, ref string text, ref Vector2 position)
         {
-            if (!Config.ModEnabled || LocalizedContentManager.CurrentLanguageCode != LocalizedContentManager.LanguageCode.mod || LocalizedContentManager.CurrentModLanguage.LanguageCode != "ar")
+            if (!Config.ModEnabled || text?.Length == 0 || LocalizedContentManager.CurrentLanguageCode != LocalizedContentManager.LanguageCode.mod || LocalizedContentManager.CurrentModLanguage.LanguageCode != "ar" || !arabicFont.Characters.Contains(text[0]))
                 return;
             string inter = "";
             for (int i = text.Length - 1; i >= 0; i--)
             {
                 inter += text[i];
+            }
+            text = inter;
+            spriteFont = arabicFont;
+        }
+        private static void FixForArabic(ref SpriteFont spriteFont, ref StringBuilder text, ref Vector2 position)
+        {
+            if (!Config.ModEnabled || text?.Length == 0 || LocalizedContentManager.CurrentLanguageCode != LocalizedContentManager.LanguageCode.mod || LocalizedContentManager.CurrentModLanguage.LanguageCode != "ar" || !arabicFont.Characters.Contains(text[0]))
+                return;
+            StringBuilder inter = new StringBuilder();
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                inter.Append(text[i]);
             }
             text = inter;
             spriteFont = arabicFont;
