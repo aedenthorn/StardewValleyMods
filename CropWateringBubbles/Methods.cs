d961e1e2e10126f1030d31b53f6df9aca9b4519b
Karson Nguyen <karsonn@sfu.ca>
2024-03-30 17:42:43 -0700

    update to 1.6

--- CropWateringBubbles/Methods.cs
+++ CropWateringBubbles/Methods.cs
@@ -60,9 +60,13 @@ namespace CropWateringBubbles
         {
             if (!Config.IncludeGiantable)
                 return false;
-            int indexOfHarvest = instance.crop.indexOfHarvest.Value;
+            string indexOfHarvest = instance.crop.indexOfHarvest.Value;
 
-            if (indexOfHarvest != 276 && indexOfHarvest != 190 && indexOfHarvest != 254)
+            if (indexOfHarvest != "190" &&
+                indexOfHarvest != "254" && 
+                indexOfHarvest != "276" && 
+                indexOfHarvest != "Powdermelon" && 
+                indexOfHarvest != "889")
                 return false;
 
             for(int x = -1; x < 2; x++)
@@ -76,129 +80,12 @@ namespace CropWateringBubbles
                 }
             }
             return true;
-
-            bool l = IsAdjacentToSame(instance, -1, 0);
-            bool u = IsAdjacentToSame(instance, 0, -1);
-            bool r = IsAdjacentToSame(instance, 1, 0);
-            bool d = IsAdjacentToSame(instance, 0, 1);
-            if (!(l && u) && !(l && d) && !(r && u) && !(r && d))
-                return false;
-            bool ul = IsAdjacentToSame(instance, -1, -1);
-            bool ur = IsAdjacentToSame(instance, 1, -1);
-            bool dr = IsAdjacentToSame(instance, 1, 1);
-            bool dl = IsAdjacentToSame(instance, -1, 1);
-
-            if(d && l && u && r && dr && dl && ur && ul)
-            {
-                return true;
-            }
-
-            if (u && ul && l)
-            {
-                if (
-                    IsAdjacentToSame(instance, -2, 0) &&
-                    IsAdjacentToSame(instance, -2, -1) &&
-                    IsAdjacentToSame(instance, -2, -2) &&
-                    IsAdjacentToSame(instance, -1, -2) &&
-                    IsAdjacentToSame(instance, 0, -2)
-                )
-                {
-                    return true;
-                }
-            }
-            if (u && ur && r)
-            {
-                if (
-                    IsAdjacentToSame(instance, 2, 0) &&
-                    IsAdjacentToSame(instance, 2, -1) &&
-                    IsAdjacentToSame(instance, 2, -2) &&
-                    IsAdjacentToSame(instance, 1, -2) &&
-                    IsAdjacentToSame(instance, 0, -2)
-                )
-                {
-                    return true;
-                }
-            }
-            if (d && dl && l)
-            {
-                if (
-                    IsAdjacentToSame(instance, -2, 0) &&
-                    IsAdjacentToSame(instance, -2, 1) &&
-                    IsAdjacentToSame(instance, -2, 2) &&
-                    IsAdjacentToSame(instance, -1, 2) &&
-                    IsAdjacentToSame(instance, 0, 2)
-                )
-                {
-                    return true;
-                }
-            }
-            if (d && dr && r)
-            {
-                if (
-                    IsAdjacentToSame(instance, 2, 0) &&
-                    IsAdjacentToSame(instance, 2, 1) &&
-                    IsAdjacentToSame(instance, 2, 2) &&
-                    IsAdjacentToSame(instance, 1, 2) &&
-                    IsAdjacentToSame(instance, 0, 2)
-                )
-                {
-                    return true;
-                }
-            }
-            if(u && l && r && ul && ur)
-            {
-                if (
-                    IsAdjacentToSame(instance, -1, -2) &&
-                    IsAdjacentToSame(instance, 0, -2) &&
-                    IsAdjacentToSame(instance, 1, -2)
-                )
-                {
-                    return true;
-                }
-
-            }
-            if(d && l && r && dl && dr)
-            {
-                if (
-                    IsAdjacentToSame(instance, -1, 2) &&
-                    IsAdjacentToSame(instance, 0, 2) &&
-                    IsAdjacentToSame(instance, 1, 2)
-                )
-                {
-                    return true;
-                }
-
-            }
-            if(u && d & l && dl && ul)
-            {
-                if (
-                    IsAdjacentToSame(instance, -2, -1) &&
-                    IsAdjacentToSame(instance, -2, 0) &&
-                    IsAdjacentToSame(instance, -2, 1)
-                )
-                {
-                    return true;
-                }
-
-            }
-            if(u && d & r && dr && ur)
-            {
-                if (
-                    IsAdjacentToSame(instance, 2, -1) &&
-                    IsAdjacentToSame(instance, 2, 0) &&
-                    IsAdjacentToSame(instance, 2, 1)
-                )
-                {
-                    return true;
-                }
-
-            }
-            return false;
         }
 
         public static bool IsAdjacentToSame(HoeDirt instance, int v1, int v2)
         {
-            return instance.currentLocation.terrainFeatures.TryGetValue(instance.currentTileLocation + new Vector2(v1, v2), out var tf) && tf is HoeDirt && (tf as HoeDirt).crop?.indexOfHarvest?.Value == instance.crop.indexOfHarvest.Value && (tf as HoeDirt).crop.currentPhase.Value >= (tf as HoeDirt).crop.phaseDays.Count - 1 && (!instance.crop.fullyGrown.Value || (tf as HoeDirt).crop.dayOfCurrentPhase.Value <= 0);
+            return instance.Location.terrainFeatures.TryGetValue(instance.Tile + new Vector2(v1, v2), 
+            out var tf) && tf is HoeDirt && (tf as HoeDirt).crop?.indexOfHarvest?.Value == instance.crop.indexOfHarvest.Value && (tf as HoeDirt).crop.currentPhase.Value >= (tf as HoeDirt).crop.phaseDays.Count - 1 && (!instance.crop.fullyGrown.Value || (tf as HoeDirt).crop.dayOfCurrentPhase.Value <= 0);
         }
     }
 }
