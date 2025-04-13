1dfedb65ae3298e2c9a787bb5c2a825a5565cd24
mouahrara <mouahrara@users.noreply.github.com>
2025-04-13 04:50:04 +0200

    Custom Starter Furniture 0.1.1

--- CustomStarterFurniture/Methods.cs
+++ CustomStarterFurniture/Methods.cs
@@ -1,68 +1,112 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewValley;
+using StardewValley.GameData.BigCraftables;
+using StardewValley.GameData.Objects;
 using StardewValley.Objects;
-using StardewValley.Tools;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using Object = StardewValley.Object;
 
 namespace CustomStarterFurniture
 {
-    public partial class ModEntry
-    {
-        private static bool IsFarm(int farmType)
-        {
-            return Game1.whichFarm == farmType || farmType < 0;
-        }
+	public partial class ModEntry
+	{
+		private static bool IsFarm(int farmType)
+		{
+			return Game1.whichFarm == farmType || farmType < 0;
+		}
 
-        private static int GetObjectIndex(string nameOrIndex, string dictPath)
-        {
-            int index = -1;
-            if (!int.TryParse(nameOrIndex, out index))
-            {
-                var dict = SHelper.GameContent.Load<Dictionary<int, string>>(dictPath);
-                try
-                {
-                    index = dict.First(p => p.Value.StartsWith(nameOrIndex + "/")).Key;
-                }
-                catch
-                {
-                    SMonitor.Log($"Object {nameOrIndex} not found in {dictPath}", StardewModdingAPI.LogLevel.Warn);
-                }
-            }
-            return index;
-        }
-        private static Object GetObject(string nameOrIndex, string itemType)
-        {
-            int index = -1;
-            Object obj = null;
-            switch (itemType)
-            {
-                case "Object":
-                    index = GetObjectIndex(nameOrIndex, "Data/ObjectInformation");
-                    if (index != -1)
-                    {
-                        obj = new Object(index, 1);
-                    }
-                    break;
-                case "Furniture":
-                    index = GetObjectIndex(nameOrIndex, "Data/Furniture");
-                    if (index != -1)
-                    {
-                        obj = new Object(index, 1);
-                    }
-                    obj = Furniture.GetFurnitureInstance(index, Vector2.Zero);
-                    break;
-                default:
-                    SMonitor.Log($"Object type {itemType} not recognized", StardewModdingAPI.LogLevel.Warn);
-                    break;
-            }
-            return obj;
-        }
-    }
+		private static Furniture GetFurniture(string nameOrIndex, int x, int y)
+		{
+			string itemId = GetFurnitureItemId(nameOrIndex);
+
+			if (itemId is not null)
+			{
+				return Furniture.GetFurnitureInstance(itemId, new Vector2(x, y));
+			}
+			SMonitor.Log($"Furniture {nameOrIndex} not found", LogLevel.Warn);
+			return null;
+		}
+
+		private static Object GetBigCraftable(string nameOrIndex, int x, int y)
+		{
+			string itemId = GetBigCraftableItemId(nameOrIndex);
+
+			if (itemId is not null)
+			{
+				return new Object(new Vector2(x, y), itemId);
+			}
+			SMonitor.Log($"BigCraftable {nameOrIndex} not found", LogLevel.Warn);
+			return null;
+		}
+
+		private static string GetObjectItemId(string nameOrIndex)
+		{
+			IDictionary<string, ObjectData> dictionary = Game1.objectData;
+
+			foreach (KeyValuePair<string, ObjectData> data in dictionary)
+			{
+				if (data.Key == $"(O){nameOrIndex}" || data.Key == nameOrIndex || data.Value.Name == nameOrIndex)
+				{
+					return data.Key;
+				}
+			}
+			return null;
+		}
+
+		private static string GetFurnitureItemId(string nameOrIndex)
+		{
+			IDictionary<string, string> dictionary = SHelper.GameContent.Load<Dictionary<string, string>>("Data/Furniture");
+
+			foreach (KeyValuePair<string, string> data in dictionary)
+			{
+				if (data.Key == $"(F){nameOrIndex}" || data.Key == nameOrIndex || data.Value.Split('/')[0] == nameOrIndex)
+				{
+					return data.Key;
+				}
+			}
+			return null;
+		}
+
+		private static string GetBigCraftableItemId(string nameOrIndex)
+		{
+			IDictionary<string, BigCraftableData> dictionary = Game1.bigCraftableData;
+
+			foreach (KeyValuePair<string, BigCraftableData> data in dictionary)
+			{
+				if (data.Key == $"(BC){nameOrIndex}" || data.Key == nameOrIndex || data.Value.Name == nameOrIndex)
+				{
+					return data.Key;
+				}
+			}
+			return null;
+		}
+
+		private static Object GetObject(string nameOrIndex, string itemType)
+		{
+			switch (itemType)
+			{
+				case "Object":
+					string objectItemId = GetObjectItemId(nameOrIndex);
+
+					if (objectItemId is not null)
+					{
+						return new Object(objectItemId, 1);
+					}
+					break;
+				case "Furniture":
+					string furnitureItemId = GetFurnitureItemId(nameOrIndex);
+
+					if (furnitureItemId is not null)
+					{
+						return Furniture.GetFurnitureInstance(furnitureItemId, Vector2.Zero);
+					}
+					break;
+				default:
+					SMonitor.Log($"Object type {itemType} not recognized", LogLevel.Warn);
+					break;
+			}
+			return null;
+		}
+	}
 }
