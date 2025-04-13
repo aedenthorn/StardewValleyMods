390bbb3800638c7730baaff4521fc95077de3508
mouahrara <mouahrara@users.noreply.github.com>
2025-04-13 04:45:43 +0200

    Crop Watering Bubbles 0.2.5

--- CropWateringBubbles/ModConfig.cs
+++ CropWateringBubbles/ModConfig.cs
@@ -1,19 +1,17 @@
-﻿using Microsoft.Xna.Framework;
-using StardewModdingAPI;
+﻿using StardewModdingAPI;
 using StardewModdingAPI.Utilities;
-using System.Collections.Generic;
 
 namespace CropWateringBubbles
 {
-    public class ModConfig
-    {
-        public bool ModEnabled { get; set; } = true;
-        public bool RequireKeyPress { get; set; } = false;
-        public bool OnlyWhenWatering { get; set; } = false;
-        public bool IncludeGiantable { get; set; } = true;
-        public int RepeatInterval { get; set; } = 3;
-        public int OpacityPercent { get; set; } = 75;
-        public int SizePercent { get; set; } = 100;
-        public KeybindList PressKeys { get; set; } = new KeybindList(new Keybind(SButton.LeftShift, SButton.OemPeriod));
-    }
+	public class ModConfig
+	{
+		public bool ModEnabled { get; set; } = true;
+		public bool RequireKeyPress { get; set; } = false;
+		public bool OnlyWhenWatering { get; set; } = false;
+		public bool IncludeGiantable { get; set; } = true;
+		public int RepeatInterval { get; set; } = 3;
+		public int OpacityPercent { get; set; } = 75;
+		public int SizePercent { get; set; } = 100;
+		public KeybindList PressKeys { get; set; } = new KeybindList(new Keybind(SButton.LeftShift, SButton.OemPeriod));
+	}
 }
