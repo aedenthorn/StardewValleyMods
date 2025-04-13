08628765a0a6330304491248b81e211ea0127489
mouahrara <mouahrara@users.noreply.github.com>
2025-04-13 06:23:13 +0200

    Watering Can Tweaks 0.3.2

--- WateringCanTweaks/CodePatches.cs
+++ WateringCanTweaks/CodePatches.cs
@@ -1,181 +1,221 @@
-﻿using HarmonyLib;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
 using Microsoft.Xna.Framework;
 using StardewValley;
+using StardewValley.Enchantments;
 using StardewValley.Objects;
 using StardewValley.TerrainFeatures;
 using StardewValley.Tools;
-using System;
-using System.Collections.Generic;
 using Object = StardewValley.Object;
 
 namespace WateringCanTweaks
 {
-    public partial class ModEntry
-    {
-        public static int tileCount = 0;
-        public static int wateredTiles = 0;
-        public static bool Tool_draw_Prefix(Tool __instance)
-        {
-            return (!Config.EnableMod || !Config.FillAdjacent || __instance is not WateringCan || __instance.UpgradeLevel == 0);
-        }
-        public static bool WateringCan_DoFunction_Prefix(WateringCan __instance, GameLocation location, int x, int y, int power, Farmer who)
-        {
-            if (!Config.EnableMod || !Config.FillAdjacent || Game1.currentLocation.CanRefillWateringCanOnTile(x / 64, y / 64) || who.toolPower == 0)
-                return true;
-            var ptr = AccessTools.Method(typeof(Tool), nameof(Tool.DoFunction)).MethodHandle.GetFunctionPointer();
-            var baseMethod = (Action<GameLocation, int, int, int, Farmer>)Activator.CreateInstance(typeof(Action<GameLocation, int, int, int, Farmer>), __instance, ptr);
-            baseMethod(location, x, y, power, who);
-            who.stopJittering();
+	public partial class ModEntry
+	{
+		public class Tool_draw_Patch
+		{
+			public static bool Prefix(Tool __instance)
+			{
+				return !Config.ModEnabled || !Config.WaterAdjacentTiles || __instance is not WateringCan || __instance.UpgradeLevel == 0;
+			}
+		}
 
-            return false;
-        }
-        public static void WateringCan_DoFunction_Postfix(WateringCan __instance, int x, int y, Farmer who)
-        {
-            if (!Config.EnableMod || !Game1.currentLocation.CanRefillWateringCanOnTile(x / 64, y / 64))
-                return;
-            __instance.waterCanMax = (int)(__instance.waterCanMax * Config.VolumeMult);
-            AccessTools.FieldRefAccess<WateringCan, int>(__instance, "waterLeft") = __instance.waterCanMax;
-            SMonitor.Log($"Filled watering can to {__instance.waterCanMax}");
-        }
-        public static bool Farmer_toolPowerIncrease_Prefix(Farmer __instance, ref int ___toolPitchAccumulator)
-        {
-            if (!Config.EnableMod || !Config.FillAdjacent || __instance.CurrentTool is not WateringCan || __instance.CurrentTool.UpgradeLevel == 0)
-                return true;
+		public class WateringCan_DoFunction_Patch
+		{
+			public static bool Prefix(WateringCan __instance, GameLocation location, int x, int y, int power, Farmer who)
+			{
+				if (!Config.ModEnabled || !Config.WaterAdjacentTiles || Game1.currentLocation.CanRefillWateringCanOnTile(x / Game1.tileSize, y / Game1.tileSize) || who.toolPower.Value == 0)
+					return true;
 
-            if (AccessTools.FieldRefAccess<WateringCan, int>(__instance.CurrentTool as WateringCan, "waterLeft") <= 0)
-                return false;
+				IntPtr ptr = AccessTools.Method(typeof(Tool), nameof(Tool.DoFunction)).MethodHandle.GetFunctionPointer();
+				Action<GameLocation, int, int, int, Farmer> baseMethod = (Action<GameLocation, int, int, int, Farmer>)Activator.CreateInstance(typeof(Action<GameLocation, int, int, int, Farmer>), __instance, ptr);
 
-            SMonitor.Log($"Tool power {__instance.toolPower}");
+				baseMethod(location, x, y, power, who);
+				who.stopJittering();
+				return false;
+			}
 
-            int level = __instance.CurrentTool.UpgradeLevel;
+			public static void Postfix(WateringCan __instance, int x, int y)
+			{
+				if (!Config.ModEnabled || !Game1.currentLocation.CanRefillWateringCanOnTile(x / Game1.tileSize, y / Game1.tileSize))
+					return;
 
-            switch (__instance.toolPower)
-            {
-                case 0:
-                    ___toolPitchAccumulator = 0;
-                    tileCount = (int)(3 * Config.WaterMult);
-                    break;
-                case 1:
-                    tileCount = (int)(2 * Config.WaterMult);
-                    break;
-                case 2:
-                    tileCount = (int)(4 * Config.WaterMult);
-                    break;
-                case 3:
-                    tileCount = (int)(9 * Config.WaterMult);
-                    break;
-            }
+				typeof(WateringCan).GetMethod("OnUpgradeLevelChanged", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(__instance, null);
+				__instance.waterCanMax = Math.Max(1, (int)(__instance.waterCanMax * Config.VolumeMultiplier));
+				__instance.WaterLeft = __instance.waterCanMax;
+				SMonitor.Log($"Filled watering can to {__instance.waterCanMax}");
+			}
+		}
 
+		public class Farmer_canStrafeForToolUse_Patch
+		{
+			public static void Postfix(Farmer __instance, ref bool __result)
+			{
+				if (!Config.ModEnabled || !Config.WaterAdjacentTiles || __instance.CurrentTool is not WateringCan)
+					return;
 
-            Vector2 startTile = new Vector2((int)__instance.GetToolLocation().X  / 64, (int)__instance.GetToolLocation().Y  / 64);
+				__result = Config.StrafeWhileWatering;
+			}
+		}
 
-            SMonitor.Log($"Trying to water tiles starting at {startTile}");
+		public class Farmer_toolPowerIncrease_Patch
+		{
+			public static bool Prefix(Farmer __instance, ref int ___toolPitchAccumulator)
+			{
+				if (!Config.ModEnabled || !Config.WaterAdjacentTiles || __instance.CurrentTool is not WateringCan wateringCan || __instance.CurrentTool.UpgradeLevel == 0)
+					return true;
 
-            HoeDirt dirt = GetHoeDirt(__instance.currentLocation, startTile);
-            if (__instance.CurrentTool.UpgradeLevel <= __instance.toolPower || (__instance.CurrentTool as WateringCan).WaterLeft <= 0 || dirt == null)
-                return false;
+				if (wateringCan.WaterLeft > 0 || __instance.hasWateringCanEnchantment)
+				{
+					int tileCount = 0;
 
-            var wateredTiles = 0;
+					SMonitor.Log($"Tool power {__instance.toolPower}");
+					switch (__instance.toolPower.Value)
+					{
+						case 0:
+							___toolPitchAccumulator = 0;
+							tileCount = (int)(3 * Config.WateringTileMultiplier);
+							break;
+						case 1:
+							tileCount = (int)(2 * Config.WateringTileMultiplier);
+							break;
+						case 2:
+							tileCount = (int)(4 * Config.WateringTileMultiplier);
+							break;
+						case 3:
+							tileCount = (int)((!wateringCan.hasEnchantmentOfType<ReachingToolEnchantment>() ? 9 : 16) * Config.WateringTileMultiplier);
+							break;
+					}
 
-            SMonitor.Log($"Trying to water {tileCount} tiles");
+					Vector2 startTile = new((int)__instance.GetToolLocation().X / Game1.tileSize, (int)__instance.GetToolLocation().Y / Game1.tileSize);
+					(HoeDirt hoeDirt, Type type) = GetHoeDirt(__instance.currentLocation, startTile);
 
-            List<Vector2> tiles = new();
-            bool empty = false;
-            if(dirt.state.Value == 0)
-            {
-                empty = !WaterHoeDirt(__instance, startTile);
-                wateredTiles++;
+					SMonitor.Log($"Trying to water tiles starting at {startTile}");
+					if (wateringCan.UpgradeLevel > __instance.toolPower.Value && hoeDirt is not null)
+					{
+						int wateredTiles = 0;
+						List<Vector2> tiles = new();
+						bool empty = false;
 
-            }
-            WaterTiles(__instance, tiles, startTile);
+						SMonitor.Log($"Trying to water {tileCount} tiles");
+						if (hoeDirt.state.Value == 0)
+						{
+							empty = !WaterHoeDirt(__instance, startTile, type);
+							wateredTiles++;
 
-            tiles.Sort(delegate(Vector2 a, Vector2 b)
-            {
-                return Vector2.Distance(startTile, a).CompareTo(Vector2.Distance(startTile, b));
-            });
-            foreach (var tile in tiles)
-            {
-                if (empty || wateredTiles >= tileCount)
-                    break;
-                dirt = GetHoeDirt(__instance.currentLocation, tile);
+						}
+						WaterTiles(__instance, tiles, startTile, type);
+						tiles.Sort(delegate(Vector2 a, Vector2 b)
+						{
+							return Vector2.Distance(startTile, a).CompareTo(Vector2.Distance(startTile, b));
+						});
+						foreach (Vector2 tile in tiles)
+						{
+							if (empty || wateredTiles >= tileCount)
+								break;
 
-                if (dirt.state.Value == 0)
-                {
-                    empty = !WaterHoeDirt(__instance, tile);
-                    wateredTiles++;
-                }
-            }
+							hoeDirt = GetHoeDirt(__instance.currentLocation, tile, type).Item1;
+							if (hoeDirt.state.Value == 0)
+							{
+								empty = !WaterHoeDirt(__instance, tile, type);
+								wateredTiles++;
+							}
+						}
+						if (wateredTiles > 0)
+						{
+							if (__instance.ShouldHandleAnimationSound())
+							{
+								__instance.currentLocation.localSound("wateringCan");
+							}
+							SMonitor.Log($"watered {wateredTiles} tiles");
+						}
+						if (!wateringCan.IsEfficient)
+						{
+							float oldStamina = __instance.Stamina;
+							float staminaUsed = ((2 * (__instance.toolPower.Value + 1)) - __instance.FarmingLevel * 0.1f) * Config.StaminaUseMultiplier;
 
-            if (wateredTiles > 0)
-            {
-                if (__instance.ShouldHandleAnimationSound())
-                {
-                    __instance.currentLocation.localSound("wateringCan");
-                }
-                SMonitor.Log($"watered {wateredTiles} tiles");
+							__instance.Stamina = Math.Max(0, __instance.Stamina - staminaUsed);
+							__instance.checkForExhaustion(oldStamina);
+							SMonitor.Log($"Used {staminaUsed} stamina");
+						}
+						SMonitor.Log($"Increasing tool power to {__instance.toolPower.Value + 1}");
+						__instance.toolPower.Value++;
+						if (Config.AutoEndWateringAnimation && (wateredTiles <= 0 || __instance.toolPower.Value >= wateringCan.UpgradeLevel || wateringCan.WaterLeft <= 0 || __instance.Stamina <= 0))
+						{
+							__instance.EndUsingTool();
+							return false;
+						}
+					}
+					else
+					{
+						__instance.EndUsingTool();
+						return false;
+					}
+				}
+				return false;
+			}
 
-            }
-            if (!__instance.CurrentTool.IsEfficient)
-            {
-                var staminaUsed = ((2 * (__instance.toolPower + 1)) - __instance.FarmingLevel * 0.1f) * Config.ChargedStaminaMult;
-                __instance.Stamina -= staminaUsed;
-                SMonitor.Log($"Used {staminaUsed} stamina");
-            }
-            SMonitor.Log($"Increasing tool power to {__instance.toolPower + 1}");
-            __instance.toolPower++;
-            return false;
-        }
+			private static void WaterTiles(Farmer farmer, List<Vector2> tiles, Vector2 startTile, Type type)
+			{
+				List<Vector2> adjacents = Utility.getAdjacentTileLocations(startTile);
 
-        private static void WaterTiles(Farmer farmer, List<Vector2> tiles, Vector2 startTile)
-        {
-            var adjacents = Utility.getAdjacentTileLocations(startTile);
-            for(int i = adjacents.Count - 1; i >= 0; i--)
-            {
-                HoeDirt dirt = GetHoeDirt(farmer.currentLocation, adjacents[i]);
-                if (tiles.Contains(adjacents[i]) || dirt is null)
-                {
-                    adjacents.RemoveAt(i);
-                    continue;
-                }
-                tiles.Add(adjacents[i]);
-            }
-            foreach(var tile in adjacents)
-            {
-                WaterTiles(farmer, tiles, tile);
-            }
-        }
+				for (int i = adjacents.Count - 1; i >= 0; i--)
+				{
+					HoeDirt dirt = GetHoeDirt(farmer.currentLocation, adjacents[i], type).Item1;
 
-        private static HoeDirt GetHoeDirt(GameLocation location, Vector2 tile)
-        {
-            if (location.terrainFeatures.TryGetValue(tile, out TerrainFeature feature) && feature is HoeDirt)
-                return feature as HoeDirt;
-            else if (SHelper.ModRegistry.IsLoaded("aedenthorn.ConnectedGardenPots") && location.objects.TryGetValue(tile, out Object obj) && obj is IndoorPot)
-                return (obj as IndoorPot).hoeDirt.Value;
+					if (tiles.Contains(adjacents[i]) || dirt is null)
+					{
+						adjacents.RemoveAt(i);
+					}
+					else
+					{
+						tiles.Add(adjacents[i]);
+					}
+				}
+				foreach (Vector2 tile in adjacents)
+				{
+					WaterTiles(farmer, tiles, tile, type);
+				}
+			}
 
-            return null;
-        }
-        private static bool WaterHoeDirt(Farmer f, Vector2 tile)
-        {
-            if (f.currentLocation.terrainFeatures.TryGetValue(tile, out TerrainFeature feature) && feature is HoeDirt)
-            {
-                (f.currentLocation.terrainFeatures[tile] as HoeDirt).state.Value = 1;
-            }
-            else if (SHelper.ModRegistry.IsLoaded("aedenthorn.ConnectedGardenPots") && f.currentLocation.objects.TryGetValue(tile, out Object obj) && obj is IndoorPot)
-            {
-                (obj as IndoorPot).hoeDirt.Value.state.Value = 1;
-                (obj as IndoorPot).showNextIndex.Value = true;
-            }
-            SMonitor.Log($"watered tile {tile}");
-            if (!(f.CurrentTool as WateringCan).IsBottomless && !f.hasWateringCanEnchantment)
-            {
-                AccessTools.FieldRefAccess<WateringCan, int>((f.CurrentTool as WateringCan), "waterLeft")--;
-                if (AccessTools.FieldRefAccess<WateringCan, int>((f.CurrentTool as WateringCan), "waterLeft") <= 0)
-                {
-                    SMonitor.Log("Watering can empty");
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
+			private static bool WaterHoeDirt(Farmer farmer, Vector2 tile, Type type)
+			{
+				if ((type == null || type == typeof(HoeDirt)) && farmer.currentLocation.terrainFeatures.TryGetValue(tile, out TerrainFeature feature) && feature is HoeDirt hoeDirt)
+				{
+					hoeDirt.state.Value = 1;
+				}
+				else if ((type == null || type == typeof(IndoorPot)) && CompatibilityUtility.IsConnectedGardenPotsLoaded && farmer.currentLocation.objects.TryGetValue(tile, out Object obj) && obj is IndoorPot indoorPot)
+				{
+					indoorPot.hoeDirt.Value.state.Value = 1;
+					indoorPot.showNextIndex.Value = true;
+				}
+				SMonitor.Log($"watered tile {tile}");
+				if (farmer.CurrentTool is WateringCan wateringCan && !wateringCan.IsBottomless && !farmer.hasWateringCanEnchantment)
+				{
+					wateringCan.WaterLeft--;
+					if (wateringCan.WaterLeft <= 0)
+					{
+						SMonitor.Log("Watering can empty");
+						return false;
+					}
+				}
+				return true;
+			}
+		}
+
+		private static (HoeDirt, Type) GetHoeDirt(GameLocation location, Vector2 tile, Type type = null)
+		{
+			if ((type == null || type == typeof(HoeDirt)) && location.terrainFeatures.TryGetValue(tile, out TerrainFeature feature) && feature is HoeDirt hoeDirt)
+			{
+				return (hoeDirt, typeof(HoeDirt));
+			}
+			else if ((type == null || type == typeof(IndoorPot)) && CompatibilityUtility.IsConnectedGardenPotsLoaded && location.objects.TryGetValue(tile, out Object obj) && obj is IndoorPot indoorPot)
+			{
+				return (indoorPot.hoeDirt.Value, typeof(IndoorPot));
+			}
+			return (null, null);
+		}
+	}
 }
--- WateringCanTweaks/ModEntry.cs
+++ WateringCanTweaks/ModEntry.cs
@@ -1,111 +1,127 @@
-﻿using HarmonyLib;
+﻿using System;
+using HarmonyLib;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Tools;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
 
 namespace WateringCanTweaks
 {
-    /// <summary>The mod entry point.</summary>
-    public partial class ModEntry : Mod
-    {
+	/// <summary>The mod entry point.</summary>
+	public partial class ModEntry : Mod
+	{
+		internal static IMonitor SMonitor;
+		internal static IModHelper SHelper;
+		internal static IManifest SModManifest;
+		internal static ModConfig Config;
+		internal static ModEntry context;
 
-        public static IMonitor SMonitor;
-        public static IModHelper SHelper;
-        public static ModConfig Config;
+		/// <summary>The mod entry point, called after the mod is first loaded.</summary>
+		/// <param name="helper">Provides simplified APIs for writing mods.</param>
+		public override void Entry(IModHelper helper)
+		{
+			Config = Helper.ReadConfig<ModConfig>();
 
-        private static Dictionary<string, Texture2D> textureDict = new Dictionary<string, Texture2D>();
+			context = this;
+			SMonitor = Monitor;
+			SHelper = helper;
+			SModManifest = ModManifest;
 
-        public static ModEntry context;
+			Helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
 
-        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
-        /// <param name="helper">Provides simplified APIs for writing mods.</param>
-        public override void Entry(IModHelper helper)
-        {
-            Config = Helper.ReadConfig<ModConfig>();
+			// Load Harmony patches
+			try
+			{
+				Harmony harmony = new(ModManifest.UniqueID);
 
-            if (!Config.EnableMod)
-                return;
+				harmony.Patch(
+					original: AccessTools.Method(typeof(Tool), nameof(Tool.draw), new Type[] {typeof(SpriteBatch) }),
+					prefix: new HarmonyMethod(typeof(Tool_draw_Patch), nameof(Tool_draw_Patch.Prefix))
+				);
+				harmony.Patch(
+					original: AccessTools.Method(typeof(WateringCan), nameof(WateringCan.DoFunction)),
+					prefix: new HarmonyMethod(typeof(WateringCan_DoFunction_Patch), nameof(WateringCan_DoFunction_Patch.Prefix)),
+					postfix: new HarmonyMethod(typeof(WateringCan_DoFunction_Patch), nameof(WateringCan_DoFunction_Patch.Postfix))
+				);
+				harmony.Patch(
+					original: AccessTools.Method(typeof(Farmer), nameof(Farmer.canStrafeForToolUse)),
+					postfix: new HarmonyMethod(typeof(Farmer_canStrafeForToolUse_Patch), nameof(Farmer_canStrafeForToolUse_Patch.Postfix))
+				);
+				harmony.Patch(
+					original: AccessTools.Method(typeof(Farmer), nameof(Farmer.toolPowerIncrease)),
+					prefix: new HarmonyMethod(typeof(Farmer_toolPowerIncrease_Patch), nameof(Farmer_toolPowerIncrease_Patch.Prefix))
+				);
+			}
+			catch (Exception e)
+			{
+				Monitor.Log($"Issue with Harmony patching: {e}", LogLevel.Error);
+				return;
+			}
+		}
 
-            context = this;
+		private void GameLoop_GameLaunched(object sender, GameLaunchedEventArgs e)
+		{
+			// get Generic Mod Config Menu's API (if it's installed)
+			var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+			if (configMenu is null)
+				return;
 
-            SMonitor = Monitor;
-            SHelper = helper;
+			// register mod
+			configMenu.Register(
+				mod: ModManifest,
+				reset: () => Config = new ModConfig(),
+				save: () => Helper.WriteConfig(Config)
+			);
 
-            Helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
-
-            var harmony = new Harmony(ModManifest.UniqueID);
-
-            harmony.Patch(
-               original: AccessTools.Method(typeof(Tool), nameof(Tool.draw), new Type[] {typeof(SpriteBatch) }),
-               prefix: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.Tool_draw_Prefix))
-            );
-            harmony.Patch(
-               original: AccessTools.Method(typeof(WateringCan), nameof(WateringCan.DoFunction)),
-               prefix: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.WateringCan_DoFunction_Prefix)),
-               postfix: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.WateringCan_DoFunction_Postfix))
-            );
-            harmony.Patch(
-               original: AccessTools.Method(typeof(Farmer), nameof(Farmer.toolPowerIncrease)),
-               prefix: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.Farmer_toolPowerIncrease_Prefix))
-            );
-        }
-
-        private void GameLoop_GameLaunched(object sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
-        {
-
-            // get Generic Mod Config Menu's API (if it's installed)
-            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
-            if (configMenu is null)
-                return;
-
-            // register mod
-            configMenu.Register(
-                mod: ModManifest,
-                reset: () => Config = new ModConfig(),
-                save: () => Helper.WriteConfig(Config)
-            );
-
-            configMenu.AddBoolOption(
-                mod: ModManifest,
-                name: () => "Mod Enabled?",
-                getValue: () => Config.EnableMod,
-                setValue: value => Config.EnableMod = value
-            );
-            
-            configMenu.AddBoolOption(
-                mod: ModManifest,
-                name: () => "Water Adjacent",
-                tooltip: () => "When charging, fill adjacent tiles instead of creating rectangle area",
-                getValue: () => Config.FillAdjacent,
-                setValue: value => Config.FillAdjacent = value
-            );
-
-            configMenu.AddTextOption(
-                mod: ModManifest,
-                name: () => "Volume Mult",
-                tooltip: () => "Multiply watering can volume by this amount",
-                getValue: () => "" + Config.VolumeMult,
-                setValue: delegate (string value) { try { Config.VolumeMult = float.Parse(value, CultureInfo.InvariantCulture); } catch { } }
-            );
-
-            configMenu.AddTextOption(
-                mod: ModManifest,
-                name: () => "Water Mult",
-                tooltip: () => "Multiply number of tiles to water by this amount.",
-                getValue: () => "" + Config.WaterMult,
-                setValue: delegate (string value) { try { Config.WaterMult = float.Parse(value, CultureInfo.InvariantCulture); } catch { } }
-            );
-            configMenu.AddTextOption(
-                mod: ModManifest,
-                name: () => "Charged Stamina Mult",
-                tooltip: () => "Multiply stamina used for charged watering by this amount (only if Water Adjacent is enabled)",
-                getValue: () => "" + Config.ChargedStaminaMult,
-                setValue: delegate (string value) { try { Config.ChargedStaminaMult = float.Parse(value, CultureInfo.InvariantCulture); } catch { } }
-            );
-        }
-    }
+			configMenu.AddBoolOption(
+				mod: ModManifest,
+				name: () => SHelper.Translation.Get("GMCM.ModEnabled.Name"),
+				getValue: () => Config.ModEnabled,
+				setValue: value => Config.ModEnabled = value
+			);
+			configMenu.AddBoolOption(
+				mod: ModManifest,
+				name: () => SHelper.Translation.Get("GMCM.WaterAdjacentTiles.Name"),
+				tooltip: () => SHelper.Translation.Get("GMCM.WaterAdjacentTiles.Tooltip"),
+				getValue: () => Config.WaterAdjacentTiles,
+				setValue: value => Config.WaterAdjacentTiles = value
+			);
+			configMenu.AddNumberOption(
+				mod: ModManifest,
+				name: () => SHelper.Translation.Get("GMCM.WateringTileMultiplier.Name"),
+				tooltip: () => SHelper.Translation.Get("GMCM.WateringTileMultiplier.Tooltip"),
+				getValue: () => Config.WateringTileMultiplier,
+				setValue: value => Config.WateringTileMultiplier = Math.Max(0, Math.Min(value, 1000))
+			);
+			configMenu.AddNumberOption(
+				mod: ModManifest,
+				name: () => SHelper.Translation.Get("GMCM.VolumeMultiplier.Name"),
+				tooltip: () => SHelper.Translation.Get("GMCM.VolumeMultiplier.Tooltip"),
+				getValue: () => Config.VolumeMultiplier,
+				setValue: value => Config.VolumeMultiplier = Math.Max(0, Math.Min(value, 1000))
+			);
+			configMenu.AddNumberOption(
+				mod: ModManifest,
+				name: () => SHelper.Translation.Get("GMCM.StaminaUseMultiplier.Name"),
+				tooltip: () => SHelper.Translation.Get("GMCM.StaminaUseMultiplier.Tooltip"),
+				getValue: () => Config.StaminaUseMultiplier,
+				setValue: value => Config.StaminaUseMultiplier = Math.Max(0, Math.Min(value, 1000))
+			);
+			configMenu.AddBoolOption(
+				mod: ModManifest,
+				name: () => SHelper.Translation.Get("GMCM.StrafeWhileWatering.Name"),
+				tooltip: () => SHelper.Translation.Get("GMCM.StrafeWhileWatering.Tooltip"),
+				getValue: () => Config.StrafeWhileWatering,
+				setValue: value => Config.StrafeWhileWatering = value
+			);
+			configMenu.AddBoolOption(
+				mod: ModManifest,
+				name: () => SHelper.Translation.Get("GMCM.AutoEndWateringAnimation.Name"),
+				tooltip: () => SHelper.Translation.Get("GMCM.AutoEndWateringAnimation.Tooltip"),
+				getValue: () => Config.AutoEndWateringAnimation,
+				setValue: value => Config.AutoEndWateringAnimation = value
+			);
+		}
+	}
 }
