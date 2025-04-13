9c698a3c720991e56100906daa3667d70dd72561
mouahrara <mouahrara@users.noreply.github.com>
2025-04-13 06:02:35 +0200

    Personal Traveling Cart 0.3.6

---
+++ PersonalTravelingCart/ParkedTravelingCart.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace PersonalTravelingCart
+{
+	public class ParkedTravelingCart
+	{
+		public Vector2 position;
+		public int facingDirection;
+		public string location;
+		public string whichCart;
+		public TravelingCart data;
+		public TravelingCart.DirectionData ddata;
+	}
+}
