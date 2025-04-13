8c3a13c3b82be189d582c123f0614de7cf47ae62
mouahrara <mouahrara@users.noreply.github.com>
2025-04-13 04:11:24 +0200

    Bulk Animal Purchase 0.2.2

--- BulkAnimalPurchase/ModConfig.cs
+++ BulkAnimalPurchase/ModConfig.cs
@@ -1,11 +1,28 @@
-﻿
-using StardewModdingAPI;
-
-namespace BulkAnimalPurchase
+﻿namespace BulkAnimalPurchase
 {
-    public class ModConfig
-    {
-        public bool EnableMod { get; set; } = true;
-
-    }
+	public class ModConfig
+	{
+		public bool EnableMod { get; set; } = true;
+		public int InitialFriendship { get; set; } = 0;
+		public bool AdultAnimals { get; set; } = false;
+		public int ChickenPrice { get; set; } = 800;
+		public int DuckPrice { get; set; } = 1200;
+		public int RabbitPrice { get; set; } = 8000;
+		public int CowPrice { get; set; } = 1500;
+		public int GoatPrice { get; set; } = 4000;
+		public int SheepPrice { get; set; } = 8000;
+		public int PigPrice { get; set; } = 16000;
+		public int DefaultIncubatorTime { get; set; } = 9000;
+		public int DefaultOstrichIncubatorTime { get; set; } = 15000;
+		public int DefaultSlimeIncubatorTime { get; set; } = 4000;
+		public int ChickenIncubationTime { get; set; } = 9000;
+		public int DuckIncubationTime { get; set; } = 9000;
+		public int DinosaurIncubationTime { get; set; } = 18000;
+		public int OstrichIncubationTime { get; set; } = 15000;
+		public int GreenSlimeIncubationTime { get; set; } = 4000;
+		public int BlueSlimeIncubationTime { get; set; } = 4000;
+		public int RedSlimeIncubationTime { get; set; } = 4000;
+		public int PurpleSlimeIncubationTime { get; set; } = 4000;
+		public int TigerSlimeIncubationTime { get; set; } = 4000;
+	}
 }
