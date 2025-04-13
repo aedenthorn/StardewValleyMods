85314bfaa6b61d7ee6e12a10f9ceb25ef030ae9c
mouahrara <mouahrara@users.noreply.github.com>
2025-04-13 04:37:59 +0200

    Crop Harvest Bubbles 0.3.1

--- CropHarvestBubbles/ModConfig.cs
+++ CropHarvestBubbles/ModConfig.cs
@@ -1,17 +1,15 @@
-﻿using Microsoft.Xna.Framework;
-using StardewModdingAPI;
+﻿using StardewModdingAPI;
 using StardewModdingAPI.Utilities;
-using System.Collections.Generic;
 
 namespace CropHarvestBubbles
 {
-    public class ModConfig
-    {
-        public bool ModEnabled { get; set; } = true;
-        public bool RequireKeyPress { get; set; } = false;
-        public bool IgnoreFlowers { get; set; } = true;
-        public int OpacityPercent { get; set; } = 75;
-        public int SizePercent { get; set; } = 100;
-        public KeybindList PressKeys { get; set; } = new KeybindList(SButton.None);
-    }
+	public class ModConfig
+	{
+		public bool ModEnabled { get; set; } = true;
+		public bool RequireKeyPress { get; set; } = false;
+		public bool IgnoreFlowers { get; set; } = true;
+		public int OpacityPercent { get; set; } = 75;
+		public int SizePercent { get; set; } = 100;
+		public KeybindList PressKeys { get; set; } = new KeybindList(SButton.None);
+	}
 }
