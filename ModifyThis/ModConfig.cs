6449471cdf63610e4bb877e211541ee076b9fa84
mouahrara <mouahrara@users.noreply.github.com>
2025-04-13 05:47:36 +0200

    Modify This 0.9.1

---
+++ ModifyThis/ModConfig.cs
@@ -0,0 +1,11 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+
+namespace ModifyThis
+{
+	public class ModConfig
+	{
+		public bool ModEnabled { get; set; } = true;
+		public SButton WizardKey { get; set; } = SButton.Pause;
+	}
+}
