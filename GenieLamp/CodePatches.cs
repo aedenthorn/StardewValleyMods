e2a0a86bfca071ffeead411de32aa77f7a391535
mouahrara <mouahrara@users.noreply.github.com>
2025-04-13 05:23:20 +0200

    Genie Lamp 0.1.2

--- GenieLamp/CodePatches.cs
+++ GenieLamp/CodePatches.cs
@@ -1,51 +1,41 @@
-﻿using HarmonyLib;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Newtonsoft.Json;
+﻿using System;
+using HarmonyLib;
 using StardewValley;
-using StardewValley.Extensions;
-using StardewValley.GameData;
-using StardewValley.ItemTypeDefinitions;
 using StardewValley.Menus;
-using StardewValley.Objects;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Object = StardewValley.Object;
-using Rectangle = Microsoft.Xna.Framework.Rectangle;
 
 namespace GenieLamp
 {
-    public partial class ModEntry
-    {
+	public partial class ModEntry
+	{
+		public class Object_performUseAction_Patch
+		{
+			public static bool Prefix(Object __instance)
+			{
+				if (!Config.ModEnabled || (!__instance.Name.Equals(Config.LampItem) && !__instance.QualifiedItemId.Equals(Config.LampItem)))
+					return true;
 
-        [HarmonyPatch(typeof(Object), nameof(Object.performUseAction))]
-        public class Object_performUseAction_Patch
-        {
-            public static bool Prefix(Object __instance)
-            {
-                if (!Config.ModEnabled || (!__instance.Name.Equals(Config.LampItem) && !__instance.QualifiedItemId.Equals(Config.LampItem)))
-                    return true;
-                int wishes = __instance.modData.TryGetValue(modKey, out var w) ? int.Parse(w) : 0;
-                if (wishes >= Config.WishesPerItem)
-                {
-                    Game1.playSound("cancel", null);
-                    Game1.showRedMessage(SHelper.Translation.Get("NoMoreWishes"));
-                    return true;
-                }
-                try
-                {
-                    Game1.playSound(Config.MenuSound, null);
-                }
-                catch { }
-                AccessTools.Method(typeof(ItemRegistry), "RebuildCache").Invoke(null, new object[0]);
+				int wishes = __instance.modData.TryGetValue(modKey, out var w) ? int.Parse(w) : 0;
 
-                Game1.activeClickableMenu = new ObjectPickMenu( new NamingMenu.doneNamingBehavior(delegate (string target)
-                {
-                    SpawnItem(target);
-                }), string.Format(SHelper.Translation.Get("WishMenuTitle"), Config.WishesPerItem - wishes));
-                return false;
-            }
-        }
-    }
+				if (wishes >= Config.WishesPerItem)
+				{
+					Game1.playSound("cancel", null);
+					Game1.showRedMessage(SHelper.Translation.Get("NoMoreWishes"));
+					return true;
+				}
+				try
+				{
+					Game1.playSound(Config.MenuSound, null);
+				}
+				catch { }
+				AccessTools.Method(typeof(ItemRegistry), "RebuildCache").Invoke(null, Array.Empty<object>());
+
+				Game1.activeClickableMenu = new ObjectPickMenu( new NamingMenu.doneNamingBehavior(delegate (string target)
+				{
+					SpawnItem(target);
+				}), string.Format(Config.WishesPerItem - wishes == 1 ? SHelper.Translation.Get("WishMenuTitleSingular") : SHelper.Translation.Get("WishMenuTitlePlural"), Config.WishesPerItem - wishes));
+				return false;
+			}
+		}
+	}
 }
--- GenieLamp/ModConfig.cs
+++ GenieLamp/ModConfig.cs
@@ -1,14 +1,11 @@
-﻿using StardewModdingAPI;
-using StardewModdingAPI.Utilities;
-
-namespace GenieLamp
+﻿namespace GenieLamp
 {
-    public class ModConfig
-    {
-        public bool ModEnabled { get; set; } = true;
-        public string LampItem { get; set; } = "Golden Mask";
-        public string MenuSound { get; set; } = "cowboy_explosion";
-        public string WishSound { get; set; } = "yoba";
-        public int WishesPerItem { get; set; } = 3;
-    }
+	public class ModConfig
+	{
+		public bool ModEnabled { get; set; } = true;
+		public string LampItem { get; set; } = "Golden Mask";
+		public string MenuSound { get; set; } = "cowboy_explosion";
+		public string WishSound { get; set; } = "yoba";
+		public int WishesPerItem { get; set; } = 3;
+	}
 }
