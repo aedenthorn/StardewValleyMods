29ccfad1fcdea4e14c7b41d338cd4c90aa95faea
mouahrara <mouahrara@users.noreply.github.com>
2025-04-13 06:07:15 +0200

    Statue Shorts 0.1.1

--- StatueShorts/ModConfig.cs
+++ StatueShorts/ModConfig.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.Xna.Framework;
-using StardewModdingAPI;
-using System.Collections.Generic;
-
-namespace StatueShorts
+﻿namespace StatueShorts
 {
-    public class ModConfig
-    {
-        public bool ModEnabled { get; set; } = true;
-    }
+	public class ModConfig
+	{
+		public bool ModEnabled { get; set; } = true;
+	}
 }
