c46e69db71872d87569cf75079026fd87d2880da
mouahrara <mouahrara@users.noreply.github.com>
2025-04-13 06:16:26 +0200

    Treasure Chests Expanded 0.3.1

--- TreasureChestsExpanded/ModConfig.cs
+++ TreasureChestsExpanded/ModConfig.cs
@@ -2,31 +2,32 @@
 
 namespace TreasureChestsExpanded
 {
-    public class ModConfig
-    {
-        public bool EnableMod { get; set; } = true;
-        public int MaxItems { get; set; } = 5;
-        public int ItemsBaseMaxValue { get; set; } = 100;
-        public int MinItemValue { get; set; } = 20;
-        public int MaxItemValue { get; set; } = -1;
-        public int CoinBaseMin { get; set; } = 20;
-        public int CoinBaseMax { get; set; } = 100;
-        public float IncreaseRate { get; set; } = 0.2f;
-        public Dictionary<string, int> ItemListChances { get; set; } = new Dictionary<string, int>
-        {
-            {"MeleeWeapon", 100},
-            {"Shirt", 0},
-            {"Pants", 0},
-            {"Hat", 0},
-            {"Boots", 100},
-            {"BigCraftable", 100},
-            {"Ring", 100},
-            {"Seed", 100},
-            {"Mineral", 100},
-            {"Relic", 100},
-            {"Cooking", 0},
-            {"Fish", 0},
-            {"BasicObject", 0}
-        };
-    }
+	public class ModConfig
+	{
+		public bool EnableMod { get; set; } = true;
+		public int ChanceForTreasureRoom { get; set; } = 1;
+		public int MaxItems { get; set; } = 5;
+		public int ItemsBaseMaxValue { get; set; } = 100;
+		public int MinItemValue { get; set; } = 20;
+		public int MaxItemValue { get; set; } = -1;
+		public int CoinBaseMin { get; set; } = 40;
+		public int CoinBaseMax { get; set; } = 200;
+		public float IncreaseRate { get; set; } = 0.2f;
+		public Dictionary<string, int> ItemListChances { get; set; } = new Dictionary<string, int>
+		{
+			{"MeleeWeapon", 100},
+			{"Shirt", 0},
+			{"Pants", 0},
+			{"Hat", 0},
+			{"Boots", 100},
+			{"BigCraftable", 50},
+			{"Ring", 100},
+			{"Seed", 100},
+			{"Mineral", 100},
+			{"Relic", 100},
+			{"Cooking", 0},
+			{"Fish", 0},
+			{"BasicObject", 0}
+		};
+	}
 }
